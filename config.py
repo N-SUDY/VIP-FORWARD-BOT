003593101453c5c4fc1ca0fc01888d06a5478720
RITESH <108003920+ritesh-0@users.noreply.github.com>
2023-06-14 00:59:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,17 +1,17 @@
 import os
 import logging
 class Config:                                                                   
-    API_ID = int(os.environ.get("API_ID", "21288218"))
-    API_HASH = os.environ.get("API_HASH", "dd47d5c4fbc31534aa764ef9918b3acd")       
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6021415198:AAGJ5z8r-bBK9iXzvymHHoKywAW-TDEPzCY")
+    API_ID = int(os.environ.get("API_ID", ""))
+    API_HASH = os.environ.get("API_HASH", "")       
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
     BOT_SESSION = os.environ.get("BOT_SESSION", "forwardbot")
-    OWNER_ID = os.environ.get("OWNER_ID", "6065594762")                             
-    DATABASE_URI = os.environ.get("DATABASE_URI", "mongodb+srv://FORWARD:FORWARD@cluster0.k1omlka.mongodb.net/?retryWrites=true&w=majority")  
+    OWNER_ID = os.environ.get("OWNER_ID", "")                             
+    DATABASE_URI = os.environ.get("DATABASE_URI", "")  
     DATABASE_NAME = os.environ.get("DATABASE_NAME", "Cluste0")
     COLLECTION_NAME = os.environ.get('COLLECTION_NAME', 'Data')
-    SESSION = os.environ.get("SESSION", "BQAu8HnWatwHa3606F4XRUkBvFwlsx21TwHR_Q8_p6xeOISNYUDLMqqWkxdOBexK0fl7dcLu5mqFbUNwFyyCtnROsrCZ_xKWO545jR8flIX3yi_k2WxN-sHLLEfwucY0m2rKozoGnWMkJ5OmVHJSh6EOXgRJeDf9sOdtXfFBVRTOf8k_t7_huU_aX5TJ2BSLdNkqkCwUrrtGLS_7Clm2fX4ki2XODzHKPLk0gzqA-8JYm8kbfkWEr7E16v1UKEOivCvG5Gv8H8IeRsTkk3e2BbZHug4bp7ismkrxh8GS4O-YKTvGysYMo7YFeZZ6lJ9Tv87rgqgtajIWkZOiL6pu24AAAAAWmJoYoA")   
-    TO_CHANNEL = int(os.environ.get("TO_CHANNEL", "-1001946118544"))
-    BOT_USERNAME= os.environ.get("BOT_USERNAME", "VIP_FORWARD_ROBOT")
+    SESSION = os.environ.get("SESSION", "")   
+    TO_CHANNEL = int(os.environ.get("TO_CHANNEL", ""))
+    BOT_USERNAME= os.environ.get("BOT_USERNAME", "")
     PORT = os.environ.get("PORT", "8080")
     WEBHOOK = bool(os.environ.get("WEBHOOK", True)) # for web support on/off
 
